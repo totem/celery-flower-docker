1666c886457616db29ded6b9b83bf41a8a369f09
sukrit007 <sukrit007@gmail.com>
2015-01-26 18:14:11 -0700

    Split default env config

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,3 +1,10 @@
 import os
-BROKER_URL = os.getenv('BROKER_URL',
-                       'amqp://guest:guest@172.17.42.1:5672')
+AMPQ_USERNAME = os.getenv('AMPQ_USERNAME', 'guest')
+AMPQ_PASSWORD = os.getenv('AMPQ_PASSWORD', 'guest')
+AMQP_HOST = os.getenv('AMPQ_HOST', '172.17.42.1')
+AMQP_PORT = int(os.getenv('AMPQ_PORT', '5672'))
+
+DEFAULT_BROKER_URL = 'amqp://%s:%s@%s:%d'\
+                     % (AMPQ_USERNAME, AMPQ_PASSWORD, AMQP_HOST, AMQP_PORT)
+
+BROKER_URL = os.getenv('BROKER_URL', DEFAULT_BROKER_URL)
--- flowerconfig.py
+++ flowerconfig.py
@@ -1,6 +1,18 @@
 import os
-port = int(os.getenv('port', '5555'))
-broker_api = os.getenv('FLOWER_BROKER_API',
-                       'http://guest:guest@172.17.42.1:15672/api/')
+
+AMPQ_ADMIN_USERNAME = os.getenv('AMPQ_ADMIN_USERNAME', 'guest')
+AMPQ_ADMIN_PASSWORD = os.getenv('AMPQ_ADMIN_PASSWORD', 'guest')
+AMQP_ADMIN_HOST = os.getenv('AMQP_ADMIN_HOST', '172.17.42.1')
+AMQP_ADMIN_PORT = int(os.getenv('AMQP_ADMIN_PORT', '5672'))
+
+DEFAULT_BROKER_API = 'http://%s:%s@%s:%d/api/' \
+                     % (AMPQ_ADMIN_USERNAME, AMPQ_ADMIN_PASSWORD,
+                        AMQP_ADMIN_HOST, AMQP_ADMIN_PORT)
+FLOWER_USERNAME = os.getenv('FLOWER_USERNAME', 'root')
+FLOWER_PASSWORD = os.getenv('FLOWER_PASSWORD', 'changeit')
+
+port = int(os.getenv('FLOWER_PORT', '5555'))
+broker_api = os.getenv('FLOWER_BROKER_API', DEFAULT_BROKER_API)
 max_tasks = int(os.getenv('FLOWER_MAX_TASKS', '3600'))
-basic_auth = [os.getenv('FLOWER_BASIC_AUTH', 'root:changeit')]
+basic_auth = [os.getenv('FLOWER_BASIC_AUTH', '%s:%s'
+                        % (FLOWER_USERNAME, FLOWER_PASSWORD))]
