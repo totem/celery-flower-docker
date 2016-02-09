b400be73feba0b571ac6453841426db9a78dfa00
Sukrit Khera <sukrit007@gmail.com>
2016-02-09 08:59:54 -0700

    Remove FLOWER_ prefix for non flower based vars

--- flowerconfig.py
+++ flowerconfig.py
@@ -8,11 +8,11 @@ AMQP_ADMIN_PORT = int(os.getenv('AMQP_ADMIN_PORT', '15672'))
 DEFAULT_BROKER_API = 'http://%s:%s@%s:%d/api/' \
                      % (AMPQ_ADMIN_USERNAME, AMPQ_ADMIN_PASSWORD,
                         AMQP_ADMIN_HOST, AMQP_ADMIN_PORT)
-FLOWER_USERNAME = os.getenv('FLOWER_USERNAME', 'root')
-FLOWER_PASSWORD = os.getenv('FLOWER_PASSWORD', 'changeit')
+USERNAME = os.getenv('USERNAME', 'root')
+PASSWORD = os.getenv('PASSWORD', 'changeit')
 
 port = int(os.getenv('FLOWER_PORT', '5555'))
 broker_api = os.getenv('FLOWER_BROKER_API', DEFAULT_BROKER_API)
 max_tasks = int(os.getenv('FLOWER_MAX_TASKS', '3600'))
 basic_auth = [os.getenv('FLOWER_BASIC_AUTH', '%s:%s'
-                        % (FLOWER_USERNAME, FLOWER_PASSWORD))]
+                        % (USERNAME, PASSWORD))]
