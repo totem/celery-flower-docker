56ff547a7f1df8dbfed5d3e3d5710a1e4b6bad8d
Sukrit Khera <sukrit007@gmail.com>
2016-04-21 09:36:26 -0700

    Multi host support

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,10 +1,13 @@
 import os
-AMPQ_USERNAME = os.getenv('AMQP_USERNAME', 'guest')
-AMPQ_PASSWORD = os.getenv('AMQP_PASSWORD', 'guest')
+AMQP_USERNAME = os.getenv('AMQP_USERNAME', 'guest')
+AMQP_PASSWORD = os.getenv('AMQP_PASSWORD', 'guest')
 AMQP_HOST = os.getenv('AMQP_HOST', '172.17.42.1')
 AMQP_PORT = int(os.getenv('AMQP_PORT', '5672'))
 
-DEFAULT_BROKER_URL = 'amqp://%s:%s@%s:%d'\
-                     % (AMPQ_USERNAME, AMPQ_PASSWORD, AMQP_HOST, AMQP_PORT)
+DEFAULT_BROKER_URL = [
+    'amqp://{}:{}@{}:{}'.format(
+        AMQP_USERNAME, AMQP_PASSWORD, host.strip(), AMQP_PORT
+    ) for host in AMQP_HOST.split(',') if host.strip()
+    ]
 
 BROKER_URL = os.getenv('BROKER_URL', DEFAULT_BROKER_URL)
