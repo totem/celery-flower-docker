6a5c9c8b2b0cb7b9879b179392f4aa501a24f1c8
Sukrit Khera <sukrit007@gmail.com>
2016-04-21 00:12:05 -0700

    Fix rabbitmq discovery

--- bin/flower.sh
+++ bin/flower.sh
@@ -6,6 +6,11 @@ export HOST_IP="${HOST_IP:-$(/sbin/ip route|awk '/default/ { print $3 }')}"
 export AMQP_HOST="${AMQP_HOST:-$HOST_IP}"
 export AMQP_ADMIN_HOST="${AMQP_HOST:-$HOST_IP}"
 export DISCOVER_RABBITMQ="${DISCOVER_RABBITMQ:-false}"
+export ETCD_HOST="${ETCD_HOST:-$HOST_IP}"
+export ETCD_PORT="${ETCD_PORT:-4001}"
+export ETCD_URL="${ETCD_URL:-http://$ETCD_HOST:$ETCD_PORT}"
+export ETCDCTL="${ETCDCTL:-etcdctl --peers $ETCD_URL}"
+export ETCD_TOTEM_BASE="${ETCD_TOTEM_BASE:-/totem}"
 
 if [ "$DISCOVER_RABBITMQ" == "true" ]; then
   until $ETCDCTL cluster-health; do
